e7187ffe7ff03a4cde18685a6b92e57e9fb52f42
Triannos <34982992+Triannos@users.noreply.github.com>
2019-12-12 10:45:27 +0100

    Homepage gedoe

--- CoMamarkt/Data/Migrations/20191204124011_aaaa.Designer.cs
+++ CoMamarkt/Data/Migrations/20191204124011_aaaa.Designer.cs
@@ -4,14 +4,16 @@ using CoMamarkt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoMamarkt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191204124011_aaaa")]
+    partial class aaaa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
