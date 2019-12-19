555f9d6a873705e5f84cf2ad83d932e06bc45ae4
Brayn Geijs <brayngeijs@gmail.com>
2019-12-19 10:27:01 +0100

    Admin area views aangemaakt: edit en datails van categorie

--- CoMamarkt/Controllers/CategoriesController.cs
+++ CoMamarkt/Controllers/CategoriesController.cs
@@ -19,12 +19,12 @@ namespace CoMamarkt.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public readonly IHostingEnvironment hostingEnvironment;
+        public readonly IWebHostEnvironment webHostEnvironment;
 
-        public CategoriesController(ApplicationDbContext context, IHostingEnvironment hostingEnvironment)
+        public CategoriesController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
-            this.hostingEnvironment = hostingEnvironment;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
         // GET: Categories
@@ -110,90 +110,90 @@ namespace CoMamarkt.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var categorie = _context.Categorie.Find(id);
-            CategorieEditViewModel categorieEditViewModel = new CategorieEditViewModel
-            {
-                Id = categorie.Id,
-                Naam = categorie.Naam,
-                BestaandeBannerURL = categorie.BannerURL,
-                BestaandeImageURL = categorie.Image
-            };
+        //[HttpGet]
+        //public IActionResult Edit(int id)
+        //{
+        //    var categorie = _context.Categorie.Find(id);
+        //    CategorieEditViewModel categorieEditViewModel = new CategorieEditViewModel
+        //    {
+        //        Id = categorie.Id,
+        //        Naam = categorie.Naam,
+        //        BestaandeBannerURL = categorie.BannerURL,
+        //        BestaandeImageURL = categorie.Image
+        //    };
 
-            return View(categorieEditViewModel);
-        }
-        [HttpPost]
-        public async Task<IActionResult> FormEdit(CategorieEditViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var categorie = _context.Categorie.Find(model.Id);
-                categorie.Naam = model.Naam;
-                if (model.UploadImage != null)
-                {
-                    if (model.BestaandeImageURL != null)
-                    {
-                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, "Images", model.BestaandeImageURL);
-                        System.IO.File.Delete(filePath);
-                    }
-                    categorie.Image = ProcessUploadedImageFile(model);
+        //    return View(categorieEditViewModel);
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> FormEdit(CategorieEditViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var categorie = _context.Categorie.Find(model.Id);
+        //        categorie.Naam = model.Naam;
+        //        if (model.UploadImage != null)
+        //        {
+        //            if (model.BestaandeImageURL != null)
+        //            {
+        //                string filePath = Path.Combine(webHostEnvironment.WebRootPath, "Images", model.BestaandeImageURL);
+        //                System.IO.File.Delete(filePath);
+        //            }
+        //            categorie.Image = ProcessUploadedImageFile(model);
 
-                }
-                if (model.UploadBannerURL != null)
-                {
-                    if (model.BestaandeBannerURL != null)
-                    {
-                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, "Images", model.BestaandeBannerURL);
-                        System.IO.File.Delete(filePath);
-                    }
-                    categorie.BannerURL = ProcessUploadedBannerFile(model);
+        //        }
+        //        if (model.UploadBannerURL != null)
+        //        {
+        //            if (model.BestaandeBannerURL != null)
+        //            {
+        //                string filePath = Path.Combine(webHostEnvironment.WebRootPath, "Images", model.BestaandeBannerURL);
+        //                System.IO.File.Delete(filePath);
+        //            }
+        //            categorie.BannerURL = ProcessUploadedBannerFile(model);
 
-                }
-                _context.Update(categorie);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+        //        }
+        //        _context.Update(categorie);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
 
 
 
-            return View(model);
+        //    return View(model);
 
-        }
+        //}
 
-        private string ProcessUploadedImageFile(CategorieViewModel model)
-        {
-            string uniekImageNaam = null;
-            if (model.UploadImage != null)
-            {
-                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "Images");
-                uniekImageNaam = Guid.NewGuid().ToString() + "_" + model.UploadImage.FileName;
-                string filePath = Path.Combine(uploadsFolder, uniekImageNaam);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    model.UploadImage.CopyTo(fileStream);
-                }
-            }
+        //private string ProcessUploadedImageFile(CategorieViewModel model)
+        //{
+        //    string uniekImageNaam = null;
+        //    if (model.UploadImage != null)
+        //    {
+        //        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "Images");
+        //        uniekImageNaam = Guid.NewGuid().ToString() + "_" + model.UploadImage.FileName;
+        //        string filePath = Path.Combine(uploadsFolder, uniekImageNaam);
+        //        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            model.UploadImage.CopyTo(fileStream);
+        //        }
+        //    }
 
-            return uniekImageNaam;
-        }
-        private string ProcessUploadedBannerFile(CategorieViewModel model)
-        {
-            string uniekBannerNaam = null;
-            if (model.UploadBannerURL != null)
-            {
-                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "Images");
-                uniekBannerNaam = Guid.NewGuid().ToString() + "_" + model.UploadBannerURL.FileName;
-                string filePath = Path.Combine(uploadsFolder, uniekBannerNaam);
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    model.UploadBannerURL.CopyTo(fileStream);
-                }
-            }
+        //    return uniekImageNaam;
+        //}
+        //private string ProcessUploadedBannerFile(CategorieViewModel model)
+        //{
+        //    string uniekBannerNaam = null;
+        //    if (model.UploadBannerURL != null)
+        //    {
+        //        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "Images");
+        //        uniekBannerNaam = Guid.NewGuid().ToString() + "_" + model.UploadBannerURL.FileName;
+        //        string filePath = Path.Combine(uploadsFolder, uniekBannerNaam);
+        //        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        //        {
+        //            model.UploadBannerURL.CopyTo(fileStream);
+        //        }
+        //    }
 
-            return uniekBannerNaam;
-        }
+        //    return uniekBannerNaam;
+        //}
         // POST: Categories/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
